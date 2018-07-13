32b2bde74542857b0a2b8d385e5947a1bf1728ba
Eufrat Tsaqib <eutsat@gmail.com>
2018-07-13 15:56:58 +0700

    Fix RabbitMQ consumer

--- rabbitmq/consumer.js
+++ rabbitmq/consumer.js
@@ -2,32 +2,43 @@ const amqp = require("amqplib/callback_api");
 const WebSocket = require("ws");
 
 const wss = new WebSocket.Server({
-    port: 8080
+    port: 8090
 });
 
 amqp.connect(
     "amqp://52.221.238.228:7002",
     (err, conn) => {
-        wss.on("connection", function connection(ws) {
-            console.log("Connected with client.");
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Connected with producer.");
 
-            conn.createChannel((err, ch) => {
-                const q = "processed_img";
-                ch.assertQueue(q, { durable: false });
+            wss.on("connection", function connection(ws) {
+                console.log("Connected with client.");
 
-                let i = 0;
-                ch.consume(
-                    q,
-                    msg => {
-                        const message = msg.content.toString();
+                conn.createChannel(function(err, ch) {
+                    const ex = "processed_img";
 
-                        ws.send(message);
+                    ch.assertExchange(ex, "fanout", { durable: false });
 
-                        i++;
-                    },
-                    { noAck: true }
-                );
+                    ch.assertQueue("", { exclusive: true }, function(err, q) {
+                        console.log(
+                            " [*] Waiting for messages in %s. To exit press CTRL+C",
+                            q.queue
+                        );
+                        ch.bindQueue(q.queue, ex, "");
+
+                        ch.consume(
+                            q.queue,
+                            function(msg) {
+                                ws.send(msg.content.toString());
+                                // console.log(" [x] %s", msg.content.toString());
+                            },
+                            { noAck: true }
+                        );
+                    });
+                });
             });
-        });
+        }
     }
 );
